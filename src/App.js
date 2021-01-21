b96f12bd830f004b8b664443d6a0955e8b29769f
Wagner53426 <wagner6528@gmail.com>
2021-01-21 19:59:58 -0300

    Consumindo API de previsao do tempo usando React

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import axios from 'axios'
+
+const  API = 'https://api.hgbrasil.com/weather?woeid=455827&format=json-cors&locale=pt';
+
+export default class App extends React.Component {
+  state = {
+    city: ' ',
+    forecast: [ ]
+  };
+
+  componentDidMount() {
+    axios.get(API)
+    .then(({ data }) => {
+     this.setState({
+      city: data.results.city_name,
+      forecast: data.results.forecast
+    });
+  });
+ }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>{this.state.city}</h1>
+
+        <table className="striped centered">
+          <thead>
+            <tr>
+                <th>Data</th>
+                <th>Min</th>
+                <th>Max.</th>
+                <th>Previsão</th>
+                <th>Img</th>
+                </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.forecast.map(day => {
+                return (
+                  <tr key = {day.date}>
+                    <td>{day.date}</td>
+                    <td>{day.min}</td>
+                    <td>{day.max}</td>
+                    <td>{day.description }</td>
+                    <td>
+                       <img
+                        src={' /weather-icons/storm.svg '}
+                       alt={day.description}/>
+                       </td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
+ </div>
+    );
+  }
+}
+
